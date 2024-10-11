f3c8e328262460541bf86610f00f5fe9e25a18dc
Александр Апарин <aparinalx@yandex.ru>
2024-10-11 08:17:52 +0200

    add metrika

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Head from "next/head";
+import Script from "next/script";
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
@@ -30,6 +31,24 @@ export default function RootLayout({ children }) {
 				<meta name="yandex-verification" content="ecdbd288aa7de175" />
 			</Head>
 			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+				<Script id="metrika" nonce="XUENAJFW123">
+					{`
+					<script type="text/javascript" >
+					(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+					m[i].l=1*new Date();
+					for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+					k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+					(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+					ym(98561654, "init", {
+							clickmap:true,
+							trackLinks:true,
+							accurateTrackBounce:true
+					});
+					</script>
+					<noscript><div><img src="https://mc.yandex.ru/watch/98561654" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+					`}
+				</Script>
 				<div
 					id="toaster"
 					style={{
